eb7c2660e28d1ba83f3f5d3ac4c492c7de733ec7
Ali Faizan <alifaizan1983@gmail.com>
2023-01-22 12:39:18 +0530

    refactor the code

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -3,11 +3,30 @@ import memesData from "../memesData";
 
 export default function Meme() {
 
+  const [memeImage, setmemeImage] = useState("");
+
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg"
+  })
+
+  const [allMemeImages, setAllMemeImages] = useState(memesData);
+
+  let {name, id} = memesData.data.memes;
+
     function getRandomMeme(){
-        const memesArray = memesData.data.memes;
-        const randomNumber = Math.floor(Math.random() * memesArray.length);
-        const url = memesArray[randomNumber].url;
-        console.log(url);
+      const memesArray = memesData.data.memes;
+      const randomNumber = Math.floor(Math.random() * memesArray.length);
+      const ImageURL = memesArray[randomNumber].url;
+      name = memesArray[randomNumber].name;
+      id = memesArray[randomNumber].id;
+        setmemeImage(ImageURL);
+
+        setMeme(prevMeme => ({
+          ...prevMeme,
+          randomImage: ImageURL
+        }))
     }
 
   return (
@@ -16,6 +35,10 @@ export default function Meme() {
         <input type="text" placeholder="Top text" className="form--input" />
         <input type="text" placeholder="Bottom text" className="form--input" />
         <button type="button" onClick={getRandomMeme} className="form--button">Get a new meme image 🖼</button>
+
+        <div>
+          <img className="meme-image" src = {meme.randomImage} alt={name} key={id}/>
+        </div>
       </form>
     </main>
   );
